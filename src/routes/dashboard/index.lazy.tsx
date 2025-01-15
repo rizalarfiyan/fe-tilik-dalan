1ca86d5fb482430d79bb860d175be5f1e6399853
Rizal Arfiyan <rizal.arfiyan.23@gmail.com>
2025-01-15 23:21:02 +0700

    feat: implement modal in map marker
    Signed-off-by: Rizal Arfiyan <rizal.arfiyan.23@gmail.com>

--- src/routes/dashboard/index.lazy.tsx
+++ src/routes/dashboard/index.lazy.tsx
@@ -2,9 +2,16 @@ import { createLazyFileRoute } from '@tanstack/react-router'
 import { Marker } from 'react-leaflet/Marker'
 import { MapContainer } from 'react-leaflet/MapContainer'
 import { TileLayer } from 'react-leaflet/TileLayer'
-import { Popup } from 'react-leaflet/Popup'
-import 'leaflet/dist/leaflet.css'
+import { useMemo, useState } from 'react'
+import {
+	Dialog,
+	DialogContent,
+	DialogHeader,
+	DialogTitle,
+} from '@components/ui/dialog'
+
 import data from './data.json'
+import 'leaflet/dist/leaflet.css'
 
 export const Route = createLazyFileRoute('/dashboard/')({
 	component: RouteComponent,
@@ -18,25 +25,53 @@ interface DataJson {
 	longitude: number
 }
 
-function RouteComponent() {
+function MapMarker({ latitude, longitude, title }: DataJson) {
+	const [isOpen, setIsOpen] = useState(false)
+
 	return (
-		<MapContainer
-			center={[-7.8, 110.4]}
-			zoom={11.5}
-			className="h-screen w-screen"
-		>
-			<TileLayer
-				attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-				url="https://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}"
-				subdomains={['mt0', 'mt1', 'mt2', 'mt3']}
+		<>
+			<Dialog open={isOpen} onOpenChange={setIsOpen}>
+				<DialogContent className="max-w-3xl">
+					<DialogHeader>
+						<DialogTitle className="text-center">{title}</DialogTitle>
+					</DialogHeader>
+				</DialogContent>
+			</Dialog>
+			<Marker
+				position={[latitude, longitude]}
+				eventHandlers={{
+					click: (e) => {
+						console.log(e, title)
+						e.originalEvent.stopPropagation()
+						setIsOpen(true)
+					},
+				}}
 			/>
-			{data.map(({ id, latitude, longitude, title }: DataJson) => {
-				return (
-					<Marker key={id} position={[latitude, longitude]}>
-						<Popup>{title}</Popup>
-					</Marker>
-				)
-			})}
-		</MapContainer>
+		</>
+	)
+}
+
+function RouteComponent() {
+	const marker = useMemo(() => {
+		return data.map((val: DataJson) => {
+			return <MapMarker key={val.id} {...val} />
+		})
+	}, [])
+
+	return (
+		<div className="relative">
+			<MapContainer
+				center={[-7.8, 110.4]}
+				zoom={11.5}
+				className="h-screen w-screen"
+			>
+				<TileLayer
+					attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+					url="https://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}"
+					subdomains={['mt0', 'mt1', 'mt2', 'mt3']}
+				/>
+				{marker}
+			</MapContainer>
+		</div>
 	)
 }
