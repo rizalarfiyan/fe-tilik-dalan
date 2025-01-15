6252fdbe9747dfff16379a2d2808751cf8010787
Rizal Arfiyan <rizal.arfiyan.23@gmail.com>
2025-01-15 22:49:56 +0700

    feat: add leaflet js with data.json
    Signed-off-by: Rizal Arfiyan <rizal.arfiyan.23@gmail.com>

--- src/routes/dashboard/index.lazy.tsx
+++ src/routes/dashboard/index.lazy.tsx
@@ -1,9 +1,42 @@
 import { createLazyFileRoute } from '@tanstack/react-router'
+import { Marker } from 'react-leaflet/Marker'
+import { MapContainer } from 'react-leaflet/MapContainer'
+import { TileLayer } from 'react-leaflet/TileLayer'
+import { Popup } from 'react-leaflet/Popup'
+import 'leaflet/dist/leaflet.css'
+import data from './data.json'
 
 export const Route = createLazyFileRoute('/dashboard/')({
 	component: RouteComponent,
 })
 
+interface DataJson {
+	id: string
+	title: string
+	link: string
+	latitude: number
+	longitude: number
+}
+
 function RouteComponent() {
-	return <div>Hello "/dashboard/"!</div>
+	return (
+		<MapContainer
+			center={[-7.8, 110.4]}
+			zoom={11.5}
+			className="h-screen w-screen"
+		>
+			<TileLayer
+				attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+				url="https://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}"
+				subdomains={['mt0', 'mt1', 'mt2', 'mt3']}
+			/>
+			{data.map(({ id, latitude, longitude, title }: DataJson) => {
+				return (
+					<Marker key={id} position={[latitude, longitude]}>
+						<Popup>{title}</Popup>
+					</Marker>
+				)
+			})}
+		</MapContainer>
+	)
 }
